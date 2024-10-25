e05ebed207de20266a78f9dd210b9a8c35dd9c6f
Francis Dolan <francis.dolan@gmail.com>
2024-10-25 13:22:03 +0100

    Added GetDockerRedis helper function. Added expire param to GetDockerMysql

--- go.mod
+++ go.mod
@@ -12,7 +12,9 @@ require (
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/containerd/continuity v0.4.3 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/docker/go-connections v0.5.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/moby/sys/user v0.3.0 // indirect
@@ -20,6 +22,7 @@ require (
 	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/opencontainers/runc v1.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/redis/go-redis/v9 v9.7.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
--- testing/docker.go
+++ testing/docker.go
@@ -1,34 +1,28 @@
 package testing
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/ory/dockertest"
 	"github.com/ory/dockertest/docker"
+	"github.com/redis/go-redis/v9"
 )
 
-func GetDockerMysql(runOptions dockertest.RunOptions, hostConfig func(*docker.HostConfig)) (*dockertest.Resource, *sqlx.DB, error) {
+func GetDockerMysql(pool *dockertest.Pool, runOptions dockertest.RunOptions, hostConfig func(*docker.HostConfig), expire uint) (*dockertest.Resource, *sqlx.DB, error) {
 	var db *sqlx.DB
-
-	pool, err := dockertest.NewPool("")
-	if nil != err {
-		return nil, nil, fmt.Errorf("cannot create a new dockertest pool: %w", err)
-	}
-
-	err = pool.Client.Ping()
-	if nil != err {
-		return nil, nil, fmt.Errorf("cannot ping dockertest client: %w", err)
-	}
 
 	mysql, err := pool.RunWithOptions(&runOptions, hostConfig)
 	if nil != err {
 		return nil, nil, fmt.Errorf("cannot create dockertest mysql: %w", err)
 	}
 
+	mysql.Expire(expire)
+
 	err = pool.Retry(func() error {
 		var err error
-		db, err := sqlx.Open("mysql", fmt.Sprintf("root:secret@(localhost:%s)/mysql?parseTime=true", mysql.GetPort("3306/tcp")))
+		db, err = sqlx.Open("mysql", fmt.Sprintf("root:secret@(localhost:%s)/mysql?parseTime=true", mysql.GetPort("3306/tcp")))
 		if err != nil {
 			return err
 		}
@@ -40,4 +34,32 @@ func GetDockerMysql(runOptions dockertest.RunOptions, hostConfig func(*docker.Ho
 	}
 
 	return mysql, db, nil
+}
+
+func GetDockerRedis(pool *dockertest.Pool, runOptions dockertest.RunOptions, hostConfig func(*docker.HostConfig), expire uint) (*dockertest.Resource, *redis.Client, error) {
+	var red *redis.Client
+
+	redisResource, err := pool.RunWithOptions(&runOptions, hostConfig)
+	if nil != err {
+		return nil, nil, fmt.Errorf("cannot create dockertest redis container: %w", err)
+	}
+
+	redisResource.Expire(expire)
+
+	err = pool.Retry(func() error {
+		var err error
+		red = redis.NewClient(&redis.Options{
+			Addr:     "localhost:" + redisResource.GetPort("6379/tcp"),
+			Password: "",
+			DB:       0,
+		})
+
+		_, err = red.Ping(context.Background()).Result()
+		return err
+	})
+	if nil != err {
+		return nil, nil, fmt.Errorf("cannot open dockertest redis connection")
+	}
+
+	return redisResource, red, nil
 }
